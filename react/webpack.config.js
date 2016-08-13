b033fa1f13e43318d77106d9cbf6776c3d12e51e
ljb <2781282767@qq.com>
2016-08-13 15:59:08 +0800

    '修改'

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,12 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin'); //css单独打包
-
+var HtmlWebpackPlugin =require('html-webpack-plugin');
 var publicPath = '/dist/'; //服务器路径
 
 var plugins = [];
 plugins.push(new ExtractTextPlugin('[name].css')); //css单独打包
-plugins.push( new webpack.HotModuleReplacementPlugin());
+plugins.push(new webpack.HotModuleReplacementPlugin());
+plugins.push(new HtmlWebpackPlugin(
+    {
+        filename: '../index.html', //生成的html存放路径，相对于 path
+        template: './src/template/index.html', //html模板路径
+        hash: true,    //为静态资源生成hash值
+        chunks:'index'
+    }
+));
 module.exports = {
     entry: {
         index: [
@@ -27,14 +35,6 @@ module.exports = {
             loaders: ['react-hot','jsx?harmony','babel?presets[]=react,presets[]=es2015'],
             include: path.join(__dirname, 'src')
         }]
-        // loaders: [
-        //     // {
-        //     //     test: /\.jsx$/,
-        //     //     exclude: /^node_modules$/,
-        //     //     loaders: ['jsx', 'babel?presets[]=es2015,presets[]=react']
-        //     // }
-        //
-        // ]
     },
     plugins:plugins,
     resolve: {
